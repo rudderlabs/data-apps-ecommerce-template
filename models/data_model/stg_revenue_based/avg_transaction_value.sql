741fbc6c4f134aa6d46949f80f8886feed61cb50
Samyuktha Vijay <80803178+v-samyuktha@users.noreply.github.com>
2022-07-23 02:21:40 +0530

    Update avg_transaction_value.sql

--- models/data_model/stg_revenue_based/avg_transaction_value.sql
+++ models/data_model/stg_revenue_based/avg_transaction_value.sql
@@ -3,7 +3,7 @@ with cte_id_stitched_order_completed as
 ANALYTICS_DB.DATA_APPS_SIMULATED.{{var('id_stitcher_name')}} b 
 on (a.user_id = b.other_id and b.other_id_type = 'user_id'))
 
-select distinct main_id, avg(properties_total) as avg_transaction_value 
+select main_id, avg(properties_total) as avg_transaction_value 
 from cte_id_stitched_order_completed
 where timestamp >= '{{ var('start_date') }}' and timestamp <= '{{ var('end_date') }}' and main_id is not null
 group by main_id
