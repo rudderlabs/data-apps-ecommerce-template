5f2e4a625f7780135e01121febd8e32d6f25da43
Samyuktha Vijay <80803178+v-samyuktha@users.noreply.github.com>
2022-07-22 21:41:06 +0530

    Update median_transaction_value.sql

--- models/data_model/stg_revenue_based/median_transaction_value.sql
+++ models/data_model/stg_revenue_based/median_transaction_value.sql
@@ -1,17 +1,10 @@
 with cte_id_stitched_order_completed as 
 (select distinct b.main_id as main_id, properties_total, timestamp from {{ source('ecommerce', 'order_completed') }} a left join 
 ANALYTICS_DB.DATA_APPS_SIMULATED.{{var('id_stitcher_name')}} b 
-on (a.user_id = b.other_id and b.other_id_type = 'user_id')),
-
-cte_ordered_properties_total as 
-(select main_id, timestamp, properties_total,
-row_number() over (partition by main_id order by properties_total) as row_id,
-count(*) over(partition by main_id) as ct
-from cte_id_stitched_order_completed)
+on (a.user_id = b.other_id and b.other_id_type = 'user_id'))
 
 select distinct main_id,
-avg(properties_total) as median_transaction_value
-from cte_ordered_properties_total
-where row_id between ct/2.0 and ct/2.0 + 1
-and timestamp >= '{{ var('start_date') }}' and timestamp <= '{{ var('end_date') }}' and main_id is not null
+median(properties_total) as median_transaction_value
+from cte_id_stitched_order_completed
+where timestamp >= '{{ var('start_date') }}' and timestamp <= '{{ var('end_date') }}' and main_id is not null
 group by main_id
