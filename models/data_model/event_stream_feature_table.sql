9c4f973d88dbdb8af7f7d7cf7c6477f25ed0b998
Samyuktha Vijay <v.samyuktha@rudderstack.com>
2022-07-28 10:37:10 +0000

    Updated event_stream_feature_table.sql

--- models/data_model/event_stream_feature_table.sql
+++ models/data_model/event_stream_feature_table.sql
@@ -1,9 +1,23 @@
-select * from {{ref('stg_user_traits')}}
-union
-select * from {{ref('stg_engagement_features')}}
-union
-select * from {{ref('stg_pre_revenue_features')}}
-union
-select * from {{ref('stg_revenue_features')}}
-union
-select * from {{ref('stg_sku_based_features')}}
+{{ config( materialized = 'incremental', 
+unique_key = 'row_id' ) }} 
+select
+   main_id,
+   feature_name,
+   {{get_end_timestamp()}} as timestamp,
+   feature_value_numeric,
+   feature_value_string,
+   feature_value_array,
+   feature_type,
+   concat_ws(main_id, to_char(timestamp), feature_name) as row_id 
+from
+(
+    select * from {{ref('stg_user_traits')}}
+    union all
+    select * from {{ref('stg_engagement_features')}}
+    union all
+    select * from {{ref('stg_pre_revenue_features')}}
+    union all
+    select * from {{ref('stg_revenue_features')}}
+    union all
+    select * from {{ref('stg_sku_based_features')}}
+)
