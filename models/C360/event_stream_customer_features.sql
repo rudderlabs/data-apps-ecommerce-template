a19a6fd5c349b0eb0be4040172b20b9ff5e70fdb
Abhinav Mishra <abhinavmishra01234@gmail.com>
2022-09-12 10:26:12 +0000

    added C360

---
+++ models/C360/event_stream_customer_features.sql
@@ -0,0 +1,27 @@
+{{ config( materialized = 'incremental', 
+unique_key = 'row_id' ) }} 
+
+{% set numeric_features = dbt_utils.get_column_values(table=ref('event_stream_feature_table'), column='feature_name', where='feature_type=\'numeric\'') %}
+{% set string_features = dbt_utils.get_column_values(table=ref('event_stream_feature_table'), column='feature_name', where='feature_type=\'string\'') %}
+{% set array_features = dbt_utils.get_column_values(table=ref('event_stream_feature_table'), column='feature_name', where='feature_type=\'array\'') %}
+
+select
+main_id, timestamp,
+    concat_ws(main_id, to_char(timestamp)) as row_id,
+    {% for feature_name in numeric_features %}
+    max(case when feature_name='{{feature_name}}' then feature_value_numeric  
+                  end) as {{feature_name}},
+    {% endfor %} 
+     {% for feature_name in string_features%}
+    max(case when feature_name='{{feature_name}}' then feature_value_string  
+                  end) as {{feature_name}},
+    {% endfor %}    
+    {% for feature_name in array_features%}
+    max(case when feature_name='{{feature_name}}' then array_to_string(feature_value_array,',') 
+                  end) as {{feature_name}}
+                  {%- if not loop.last %},{% endif -%}
+    {% endfor %}   
+from {{ref('event_stream_feature_table')}}
+where timestamp = {{get_end_timestamp()}}
+group by main_id, timestamp, concat_ws(main_id, to_char(timestamp))
+
