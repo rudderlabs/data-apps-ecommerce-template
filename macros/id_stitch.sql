282877f905ea099a226660922969a88c47adeb2e
dpatchigolla <dileep@rudderstack.com>
2022-12-02 14:14:07 +0530

    Modified id stitch definition with a bug fix in case of more than one column

--- macros/id_stitch.sql
+++ macros/id_stitch.sql
@@ -7,10 +7,17 @@ FROM
    WHERE {{timebound(timestamp_col)}} 
    {% endif %} ) a
 LEFT JOIN {{ var('tbl_id_stitcher') }} b ON 
-{% for id_col in list_id_cols %} 
-        a.{{id_col}} = b.{{ var('col_id_stitcher_other_id')}} 
-        {% if not loop.last %}
-                OR 
-        {% endif %} 
-{% endfor %} 
+{% if list_id_cols|length == 1 %}
+a.{{list_id_cols|first}} = b.{{ var('col_id_stitcher_other_id')}} 
+{% else %}
+b.{{ var('col_id_stitcher_other_id')}}  = coalesce(
+    {% for id_col in list_id_cols %}
+    a.{{id_col}}
+    {% if not loop.last %}
+                , 
+    {% endif %} 
+    {% endfor %}
+)
+{% endif %}
+
 {% endmacro %}
