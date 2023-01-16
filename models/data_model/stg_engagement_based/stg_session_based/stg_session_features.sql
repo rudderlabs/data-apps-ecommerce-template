57d37576a5e13b9512106c9dfa468098e8a028a6
Sashank <sashankmvv@gmail.com>
2023-01-16 13:08:10 +0530

    minor changes in session_features

--- models/data_model/stg_engagement_based/stg_session_based/stg_session_features.sql
+++ models/data_model/stg_engagement_based/stg_session_based/stg_session_features.sql
@@ -1,16 +1,16 @@
 with cte_sessions as (
-    select * from {{ref('stg_sessions')}}
+    select * from {{ref('stg_sessions')}} where session_id is not null
 )
 
 select {{ var('main_id') }}, 
     'n_sessions_overall' as feature_name, 
-    max(session_id)+1 as feature_value 
+    count(session_id) as feature_value 
 from cte_sessions 
 group by {{ var('main_id') }}
 union
 select {{ var('main_id') }}, 
     'n_sessions_last_week' as feature_name, 
-    max(session_id)+1 as feature_value 
+    count(session_id) as feature_value 
 from cte_sessions 
 where datediff(day, date(session_start_time), date({{get_end_timestamp()}})) between 0 and 7 
 group by {{ var('main_id') }}
